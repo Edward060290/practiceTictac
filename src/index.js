be72bbc585935180303c2319357b5d61c6b3cd1c
Edward060290 <barrnoneduardo060290@gmail.com>
2018-02-12 01:24:34 -0600

    created the render() for Board Compound

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ class Square extends React.Component {
   }
 }
 
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -27,8 +28,35 @@ class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
 }
+render() {
+  const status = 'Next player: x'
+
+  return (
+  <div>
+    <div className="status"> {status}
+
+</div>
+
+    <div className="board-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+    </div>
+  </div>
+    );
+  }
 }
 
 
